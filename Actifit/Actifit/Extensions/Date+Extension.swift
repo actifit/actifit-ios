5da3ce68645806231b5b404a56e9481496b744c6
Mohammad Farhat <mcfarhat@gmail.com>
2019-12-18 19:36:37 +0200

    Implement Additional Date Helper Functions
    Implement Additional Date Helper Functions

--- Actifit/Actifit/Extensions/Date+Extension.swift
+++ Actifit/Actifit/Extensions/Date+Extension.swift
@@ -34,4 +34,34 @@ extension Date {
         formatter.dateFormat = format
         return formatter.string(from: self)
     }
+    
+    func dateTimeString(withFormat format: String = "yyyy-MM-dd hh:mm") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+    
+ 
+    func dayAfter () -> Date {
+            let oneDay:Double = 60 * 60 * 24
+            return addingTimeInterval(oneDay) as Date
+        }
+    func dayBefor () -> Date {
+            let oneDay:Double = 60 * 60 * 24
+            return addingTimeInterval(-(Double(oneDay))) as Date
+        }
+    
+    public func setTime(hour: Int, min: Int, sec: Int) -> Date? {
+        let x: Set<Calendar.Component> = [.year, .month, .day, .hour, .minute, .second]
+        let cal = Calendar.current
+        var components = cal.dateComponents(x, from: self)
+
+        //components.timeZone = NSTimeZone.local //TimeZone(abbreviation: timeZoneAbbrev)
+        components.hour = hour
+        components.minute = min
+        components.second = sec
+
+        return cal.date(from: components)
+    }
+   
 }
